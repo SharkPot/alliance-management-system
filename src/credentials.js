d7cadde1cacbdac4539feb482062065d6344d3b8
Abhinab Das <abhinabdas1996@gmail.com>
2020-12-31 16:25:10 +0530

    converted credentials to js file

---
+++ src/credentials.js
@@ -0,0 +1,6 @@
+const googleCreds = {
+    clientID:"854403770311-9dbujqpesmuimaub6i6i9m7qjo4n5b1p.apps.googleusercontent.com",
+    clientSecret:"wflMeWGnB28C_Fg-zgLc7Qm0"
+}
+
+export default googleCreds;
