ceda3d1bd766d875513d52babb9aa660b86c37f5
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-01 13:23:24 +0530

    Added 2 basic components

---
+++ src/Components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function About() {
+    return (
+        <div>
+            About
+            <div>
+                <Link to='/'>
+                    Go to home
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default About;
