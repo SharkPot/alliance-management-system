4d6d7bf7ab97f504095fca45982d4d9ac5a10dd6
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-14 19:43:39 +0530

    added some experimental to be removed code

--- src/Components/HeaderBar/index.js
+++ src/Components/HeaderBar/index.js
@@ -4,6 +4,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Fade from "@material-ui/core/Fade";
 
 import HEADER_LIST from './headers_setup';
 
@@ -23,11 +27,22 @@ const useStyles = makeStyles((theme) => ({
 	toolbarLink: {
 		padding: theme.spacing(1),
 		flexShrink: 0,
+		textDecoration: 'none'
 	},
 }));
 
 export default function Header(props) {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
 	const classes = useStyles();
+
+	const handleClick = event => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	}
 
 	return (
 		<React.Fragment>
@@ -44,24 +59,52 @@ export default function Header(props) {
         		</Typography>
 			</Toolbar>
 			<Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-				<Link
-					color="inherit"
-					noWrap
-					key={HEADER_LIST.Home.title}
-					variant="body2"
-					href={HEADER_LIST.Home.url}
-					className={classes.toolbarLink}>
-					{HEADER_LIST.Home.title}
-        		</Link>
-				<Link
-					color="inherit"
-					noWrap
-					key={HEADER_LIST.About.title}
-					variant="body2"
-					href={HEADER_LIST.About.url}
-					className={classes.toolbarLink}>
-					{HEADER_LIST.About.title}
-        		</Link>
+				<Button>
+					<Link
+						color="inherit"
+						noWrap
+						underline='none'
+						key={HEADER_LIST.Home.title}
+						variant="body2"
+						href={HEADER_LIST.Home.url}
+						className={classes.toolbarLink}>
+						{HEADER_LIST.Home.title}
+					</Link>
+				</Button>
+				<Button>
+					<Link
+						color="inherit"
+						noWrap
+						underline='none'
+						key={HEADER_LIST.About.title}
+						variant="body2"
+						href={HEADER_LIST.About.url}
+						className={classes.toolbarLink}>
+						{HEADER_LIST.About.title}
+					</Link>
+				</Button>
+				<Button onClick={handleClick}>Open</Button>
+				<Menu
+					anchorEl={anchorEl}
+					keepMounted
+					open={open}
+					onClose={handleClose}
+					TransitionComponent={Fade}
+				>
+					<MenuItem onClick={handleClose}>M1</MenuItem>
+					<MenuItem onClick={handleClose}>M2</MenuItem>
+				</Menu>
+				<Button onClick={handleClick}>Open2</Button>
+				<Menu
+					anchorEl={anchorEl}
+					keepMounted
+					open={open}
+					onClose={handleClose}
+					TransitionComponent={Fade}
+				>
+					<MenuItem onClick={handleClose}>M3</MenuItem>
+					<MenuItem onClick={handleClose}>M4</MenuItem>
+				</Menu>
 			</Toolbar>
 		</React.Fragment>
 	);
