67eece4c39c89a20c2d6b399f4ffec0dcb69f9fc
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-21 19:20:24 +0530

    removed un-used import

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
+
 
 const useStyles = makeStyles((theme) => ({
     divider:{
