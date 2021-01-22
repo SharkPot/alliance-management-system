855d2bbab1dfc85fa4776014267096301cfb1690
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-22 18:02:32 +0530

    re-created headerbar for new implementation

--- src/Components/HeaderBar/index.js
+++ src/Components/HeaderBar/index.js
@@ -1,107 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import Fade from "@material-ui/core/Fade";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import HEADER_LIST from './headers_setup';
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
-	toolbar: {
-		borderBottom: `1px solid ${theme.palette.divider}`,
-		background: theme.palette.background.paper,
-		color: theme.palette.text.primary
+	appBar: {
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+		},
 	},
-	toolbarTitle: {
-		flex: 1,
-	},
-	toolbarSecondary: {
-		justifyContent: 'space-around',
-		overflowX: 'auto',
-		background: theme.palette.grey[900],
-		color: theme.palette.text.secondary
-	},
-	toolbarLink: {
-		padding: theme.spacing(1),
-		flexShrink: 0,
-		textDecoration: 'none',
-		color: theme.palette.text.secondary
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none',
+		},
 	},
 }));
 
-export default function Header(props) {
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const open = Boolean(anchorEl);
+export default function HeaderBar(props) {
 	const classes = useStyles();
-
-	const handleClick = event => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	}
+	console.log('Header props',props);
 
 	return (
-		<React.Fragment>
-			<Toolbar className={classes.toolbar}>
-				<Typography
-					component="h2"
-					variant="h5"
-					color="inherit"
-					align="center"
-					noWrap
-					className={classes.toolbarTitle}
-				>
-					Title
-        		</Typography>
-			</Toolbar>
-			<Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-				<Button>
-					<Link to={HEADER_LIST.Home.url} className={classes.toolbarLink}>
-						{HEADER_LIST.Home.title}
-					</Link>
-				</Button>
-				<Button>
-					<Link to={HEADER_LIST.About.url}  className={classes.toolbarLink}>
-						{HEADER_LIST.About.title}
-					</Link>
-				</Button>
-				<Button onMouseEnter={handleClick}>Open</Button>
-				<Menu
-					anchorEl={anchorEl}
-					keepMounted
-					open={open}
-					onClose={handleClose}
-					MenuListProps={{ onMouseLeave: handleClose }}
-					TransitionComponent={Fade}
-				>
-					<MenuItem onClick={handleClose}>M1</MenuItem>
-					<MenuItem onClick={handleClose}>M2</MenuItem>
-				</Menu>
-				<Button onMouseEnter={handleClick}>Open2</Button>
-				<Menu
-					anchorEl={anchorEl}
-					keepMounted
-					open={open}
-					onClose={handleClose}
-					MenuListProps={{ onMouseLeave: handleClose }}
-					TransitionComponent={Fade}
-				>
-					<MenuItem onClick={handleClose}>M3</MenuItem>
-					<MenuItem onClick={handleClose}>M4</MenuItem>
-				</Menu>
-			</Toolbar>
-		</React.Fragment>
+		<div>
+			<CssBaseline />
+			<AppBar position="fixed" className={classes.appBar}>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={props.menuOnClick}
+						className={classes.menuButton}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" noWrap>
+						Responsive drawer
+          			</Typography>
+				</Toolbar>
+			</AppBar>
+		</div>
 	);
 }
-
-Header.propTypes = {
-	sections: PropTypes.array,
-	title: PropTypes.string,
-};
