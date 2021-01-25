cbe206901431577ed43255c5bca972e95ebb28ad
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-25 18:18:28 +0530

    added useffect to update title on mount

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-function About() {
+function About(props) {
+
+    useEffect(() => {
+		props.onRoute('About')
+	})
+
     return (
         <div>
             About
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
@@ -11,23 +11,17 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
-function Home() {
+function Home(props) {
 	const classes = useStyles();
+
+	useEffect(() => {
+		props.onRoute('Home')
+	})
+
 	return (
 		<div>
 			<Typography variant='subtitle1'>Para 1</Typography>
-			<Typography paragraph>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-				ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-				facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-				gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-				donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-				adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-				Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-				imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-				arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-				donec massa sapien faucibus et molestie ac.
-            </Typography>
+			<iframe src="https://calendar.google.com/calendar/embed?src=nujrnhog654g3v0m0ljmjbp790%40group.calendar.google.com&amp;mode=AGENDA&amp;color=%23616161&amp;hl=en&amp;showNav=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0" scrolling="no" width="100%" height="500" frameBorder="0"></iframe>
 			<Divider className={classes.divider} />
 			<Typography variant='subtitle1'>Para 2</Typography>
 			<Typography paragraph>
