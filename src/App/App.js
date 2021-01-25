69a364b7da1a45735bd71595016a8f772615de78
Abhinab Das <abhinabdas1996@gmail.com>
2021-01-25 16:11:33 +0530

    added routing to app component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import SideBar from '../Components/SideBar';
 import HeaderBar from '../Components/HeaderBar';
+import Router from './router';
 
 const drawerWidth = 240;
 
@@ -28,6 +30,9 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 		padding: theme.spacing(3),
 	},
+	gridPaper: {
+		padding: theme.spacing(2),
+	}
 }));
 
 function App(props) {
@@ -36,8 +41,8 @@ function App(props) {
 	const theme = useTheme();
 	const [mobileOpen, setMobileOpen] = React.useState(false);
 
-	const handleDrawerToggle = () => {
-		console.log('handleDrawerToggle called');
+	const handleDrawerToggle = (event) => {
+		console.log('handleDrawerToggle called',event);
 		setMobileOpen(!mobileOpen);
 	};
 
@@ -78,29 +83,13 @@ function App(props) {
 			</nav>
 			<main className={classes.content}>
 				<div className={classes.toolbar} />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-					facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-					gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-					donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-					adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-					Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-					imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-					arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-					donec massa sapien faucibus et molestie ac.
-        </Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-					facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-					tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-					consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-					vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-					hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-					tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-					nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-					accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+				<Grid container justify='center' alignItems='stretch'>
+					<Grid item xs={11}>
+						<Paper className={classes.gridPaper}>
+							<Router />
+						</Paper>
+					</Grid>
+				</Grid>
 			</main>
 		</div>
 	);
