adb191a75459f63310364be75baee016f07d0671
Abhinab Das <abhinabdas1996@gmail.com>
2021-02-01 20:12:21 +0530

    created class setup obj

---
+++ src/Components/ClassGuides/class_guide_setup.js
@@ -0,0 +1,156 @@
+const GUIDE_LIST = {
+    Hunter:{
+        name:'Hunter',
+        path:'/guides/hunter',
+        guide:[
+            {
+                title:'Hunter Sword Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vSKkCtYIdQ10-2ebV_-ewJxnp16GY7HHEfbWtxfLwOVJUTr-QaPikivr7lD4M_fiKnAWuxcM0NOACaQ/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1eazJ_xHTLzFge9KHNIoCcfnTg5rUylIDdBwJg6SJm3g/edit'
+            },
+            {
+                title:'Hunter Partisan Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vTh9bKWeowUgXc5UYobcIjsD3LgXSJ0J4qNhRjXeql0Jz5qa9WrtkhMAlzIWNH6vPdmIL1UWCYjg0Zu/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1efuuduqS_g22LzTypSui8ArHwjcCyMpZ11xHLB2VFMk/edit'
+            }
+        ]
+    },
+    Fighter:{
+        name:'Fighter',
+        path:'/guides/fighter',
+        guide:[
+            {
+                title:'Fighter Double Saber Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vRke7EiOGSRNyePuqyn9UHyIa6wCl8dXUTik2sveiMdTPHHqs-P4P_ZH8MpeLVSJgZ0j3MT4Q399ZJB/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1vfwSajJL5Tze6vTIyNa6jeTjLQ4IFNj24a-X9fCaur8/edit'
+            }
+        ]
+    },
+    Ranger:{
+        name:'Ranger',
+        path:'/guides/ranger',
+        guide:[
+            {
+                title:'Ranger Guide',
+                url:'https://somethingaboutlagging.wordpress.com/'
+            }
+        ]
+    },
+    Gunner:{
+        name:'Gunner',
+        path:'/guides/gunner',
+        guide:[
+            {
+                title:'Gunner Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vSpw0A-_eLT6HI8Q7BaO-fJaCRJhGCpqR4n8hMUtCieuOJt4SeXRDEJeVGDbjxVmPEKsw6trZg-cdyL/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1kfjkUQzY3QRmILocETvCqsyBiUtk_PM9n6hw6hB2y4k/edit#'
+            }
+        ]
+    },
+    Force:{
+        name:'Force',
+        path:'/guides/force',
+        guide:[
+            {
+                title:'Force Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vT2V3llb7-52h3DyT1xnV_FMP-ew50oXd-DkQCWQV5oXyuActO31_5b_ZtcjerL_rhXkMUWWrsp7tkp/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1TMO8zro-8rhB61R73-rNlg_CC5BnqzEvKgVg6vg5oxM/view'
+            }
+        ]
+    },
+    Techer:{
+        name:'Techer',
+        path:'/guides/techer',
+        guide:[
+            {
+                title:'Techer Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vR-Etn0DyO1i8UWIwkGaFT98N84-HoVySfjDxAzHwe77nwKNwVtpYlIYqMkFlNWrvD1mj9hbsNpKnNm/pub?embedded=true',
+                url:'https://docs.google.com/document/d/11N09BzAVCBpoOL9EIM5UxLZP4Ys7yO0XcdsecU0kTck/edit'
+            }
+        ]
+    },
+    Braver:{
+        name:'Braver',
+        path:'/guides/braver',
+        guide:[
+            {
+                title:'Braver Katana Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vROHCDgJWH0oNYf6skY3ayb-ZkAKkywFDcGc_Uw4o3CpVqQ6xUmBYRTX7xd22oIlqjTNrubifpr4gkO/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1rM9GvtWBB0mG9Q1-9ZsDQlTN0RqIn7SzTDjxNjgy4ls/edit'
+            },
+            {
+                title:'Braver Bow Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vRVQHFo6oGLn-bZKtx3pLCGd10uNFyBqqfpjd5xgP7qKXMSPQC2YHRV6UbKzai6d0NKrTTDaRk9wP26/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1-eK8zIf8cAkaJSfwz46CDU8yZz0RvFzewFQeRtlTfAM/edit'
+            }
+        ]
+    },
+    Bouncer:{
+        name:'Bouncer',
+        path:'/guides/bouncer',
+        guide:[
+            {
+                title:'Bouncer Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vSVcILnXiCtrSOMDNxRfbmkwVwV_o_0SvlWDAlE2BB_VcRyob56IChPGM6RXn1c43yBCRX4-rMr-g1V/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1eFdIE3fB9OgPwWU4ocDO2o8TjDUr_gLkLNweV5vVMP8/edit'
+            }
+        ]
+    },
+    Summoner:{
+        name:'Summoner',
+        path:'/guides/summoner',
+        guide:[
+            {
+                title:'Summoner Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vRP54UU567GI_B9gbRCnnF2HAZytojSCkNv-Zh3BJ7u9bzSHyQXSCHWIkHjhe3HbuVs_McXilNR1Cv0/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1HZqJp4K6fueOxABqbgl9KafdtZOJUe8jkU6E42hDXc8/edit'
+            }
+        ]
+    },
+    Hero:{
+        name:'Hero',
+        path:'/guides/hero',
+        guide:[
+            {
+                title:'Hero Guide',
+                embedUrl:'',
+                url:''
+            }
+        ]
+    },
+    Phantom:{
+        name:'Phantom',
+        path:'/guides/phantom',
+        guide:[
+            {
+                title:'Phantom Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vRchiSEzDuaRvYKrvTco0T9TrcWtpKy8OFKHem-I4XdNI2LGZv44zm8yk3ZktlM40Ivr7RlDC9TnLRi/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1HVumhu6xPdI2oB2ZvHxSM7BS-QIyoNA_KkxI7q3s0bY/edit#'
+            }
+        ]
+    },
+    Etoille:{
+        name:'Etoille',
+        path:'/guides/etoille',
+        guide:[
+            {
+                title:'Etoille Guide',
+                embedUrl:'https://docs.google.com/document/d/e/2PACX-1vR6WccFLGviBTuAEeoN0YxPtbvbOoon_tQ7-v8VlQz1TdG2aj3p5EYZVIgHTnQIGpe_0As0KsO7KI0h/pub?embedded=true',
+                url:'https://docs.google.com/document/d/1fqd9V-2fXvPqfWLi7cdzMYo07Qilq2CJY8mEQOMs8G4/edit#heading=h.sp2gekqap7jw'
+            }
+        ]
+    },
+    Luster:{
+        name:'Luster',
+        path:'/guides/luster',
+        guide:[
+            {
+                title:'Luster Guide',
+                embedUrl:'',
+                url:''
+            }
+        ]
+    },
+}
+
+export default GUIDE_LIST;
